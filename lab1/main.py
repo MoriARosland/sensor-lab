8cd9315f1d7bb08eb08d70c03b3ba02d714c5724
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-01-30 18:36:16 +0100

    add filter plot

---
+++ lab1/main.py
@@ -0,0 +1,33 @@
+import csv
+import matplotlib.pyplot as plt
+from raspi_import import raspi_import
+
+frequencies = []
+channel1_magnitude = []
+channel1_phase = []
+channel2_magnitude = []
+
+
+with open('lab1/data/filtermaling2.csv', mode='r') as file:
+    csv_reader = csv.reader(file)
+
+    next(csv_reader)  # skip first line in csv.
+
+    for row in csv_reader:
+        frequencies.append(float(row[0]))
+        channel1_magnitude.append(float(row[1]))
+        channel1_phase.append(float(row[2]))
+        channel2_magnitude.append(float(row[3]))
+
+cutoff_frequency = 24
+
+plt.plot(frequencies, channel1_magnitude)
+plt.xlabel("Frequency [Hz]")
+plt.ylabel("Magnitude [dB]")
+plt.title("Magnitude response")
+plt.axvline(x=cutoff_frequency, color='r', linestyle='--', label=f'Cutoff Frequency: ~{cutoff_frequency} Hz')
+plt.tight_layout()
+plt.xscale("log")
+plt.legend()
+
+plt.show()
