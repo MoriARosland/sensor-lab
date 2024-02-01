fix power_spectrum calculation

lab1/fft.py
@@ -2,17 +2,11 @@ import numpy as np
 
 
 def calc_normalized_power_spectrum(data, N_fft):
-    if (N_fft < len(data)):
-        print("N_fft length to low. N_fft must be greater than legth of provided data.")
-        exit(-1)
-    if not np.log2(N_fft).is_integer():
-        print(
-            f"Data lenght is: {len(data)}. The closest power to your data lenght is: {round(2**(np.ceil(np.log2(len(data)))))}.\n"
-        )
-        print("Recommended N_ftt is 2^n")
-        exit(-1)
 
-    S_x = 20*np.log10(abs(data))  # Power in decibel
+    X_f = np.fft.fft(data, N_fft)
+    X_f = np.fft.fftshift(X_f)
+
+    S_x = 20*np.log10(abs(X_f))  # Power in decibel
     S_x_normalized = S_x - np.max(S_x)
 
     return S_x_normalized
@@ -31,10 +25,3 @@ def calc_fft_windowed(data, N_fft):
     data_windowed_fft = np.fft.fft(data_windowed, N_fft)
 
     return data_windowed_fft
-
-
-def generate_freq_array(F_s, N_fft):
-    F_s = F_s / 2
-    N_fft = N_fft // 2
-
-    return np.linspace(0, F_s, N_fft)