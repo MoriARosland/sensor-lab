1eee0a4ef4b5eae27b434a1c0b5839cebc471dde
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-02-01 09:26:43 +0100

    refactor

--- lab1/main.py
+++ lab1/main.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 from raspi_import import raspi_import
+from fft import calc_fft_windowed, calc_normalized_power_spectrum, generate_freq_array
 
 frequencies = []
 channel1_magnitude = []
