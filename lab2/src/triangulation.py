f9a0d482222cdb9e7671117695ed5cc2c85d8568
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-02-28 16:39:33 +0100

    feat: triangulation code

--- lab2/src/triangulation.py
+++ lab2/src/triangulation.py
@@ -1,9 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.signal import detrend
 
 
 def calc_angle(n12, n13, n23):
-    numerator = np.sqrt(3) + n12 + n13
+    numerator = np.sqrt(3) * (n12 + n13)
     denumenator = n12 - n13 - 2 * n23
 
     angle = np.arctan(numerator / denumenator)
@@ -15,13 +16,12 @@ def calc_angle(n12, n13, n23):
 
 
 def estimate_angle(sample_period, data):
-    sample_start = 10000
+    sample_start = 10000  # Vicinitty of clap
     sample_end = 15000
 
-    for i in range(0, 3):
+    for i in range(2, 5):
         data[:, i] = detrend(data[:, i])
 
-    # Adjust just index to the corresponding microphone.
     sound = [
         data[sample_start:sample_end, 4],
         data[sample_start:sample_end, 2],
@@ -35,8 +35,14 @@ def estimate_angle(sample_period, data):
         0, len(sound[0]) * sample_period, sample_period / interpolation_factor
     )
 
+    # _, ax = plt.subplots(2)
+    # ax[0].plot(x[:50], sound[0][:50], marker=".")
+
     for i in range(3):
         sound[i] = np.interp(x_vals, x, sound[i])
+
+    # ax[1].plot(x_vals[:100], sound[0][:100], marker=".")
+    # plt.show()
 
     corr = [
         np.correlate(sound[1], sound[0], "full"),
@@ -54,16 +60,29 @@ def estimate_angle(sample_period, data):
     l = np.arange(-num_samples + 1, num_samples)
 
     for i in range(3):
-        # index of max value
         max_index = np.argmax(np.abs(corr[i]))
         delay = l[max_index]
         lags[list(lags.keys())[i]] = delay
 
         # plt.plot(l, corr[i])
+
         # plt.title(f"Correlation \nLag: {delay}")
         # plt.xlabel("Lag")
         # plt.ylabel("Amplitude")
+
         # plt.show()
+
+    # autocorr = np.correlate(sound[0], sound[0], "full")
+    # max_index = np.argmax(np.abs(autocorr))
+    # delay = l[max_index]
+    # lags["n11"] = delay
+
+    # Plot the autocorr in the same manner as the crosscorr
+    # plt.plot(l, autocorr)
+    # plt.title(f"Autocorrelation \nLag: {delay}")
+    # plt.xlabel("Lag")
+    # plt.ylabel("Amplitude")
+    # plt.show()
 
     angle = calc_angle(**lags)
 
