2f956ac5f8f6d8f3864d051e08963907ae9b882f
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-02-29 11:54:20 +0100

    feat: add script for calculating penetration depths

---
+++ lab3/src/simple_model.py
@@ -0,0 +1,55 @@
+import numpy as np
+from tabulate import tabulate
+
+
+muabo = np.genfromtxt("lab3/data/txt/muabo.txt", delimiter=",")
+muabd = np.genfromtxt("lab3/data/txt/muabd.txt", delimiter=",")
+
+# Wavelengths where color channel sensitivity is highest (determined by camera type)
+red_wavelength = 600
+green_wavelength = 515
+blue_wavelength = 460
+
+wavelength = np.array([red_wavelength, green_wavelength, blue_wavelength])
+
+
+def mua_blood_oxy(x): return np.interp(x, muabo[:, 0], muabo[:, 1])
+def mua_blood_deoxy(x): return np.interp(x, muabd[:, 0], muabd[:, 1])
+
+
+bvf = 0.01  # Blood volume fraction, average blood amount in tissue
+oxy = 0.8  # Blood oxygenation
+
+# Absorption coefficient ($\mu_a$ in lab text)
+# Units: 1/m
+mua_other = 25  # Background absorption due to collagen, et cetera
+mua_blood = (mua_blood_oxy(wavelength)*oxy  # Absorption due to
+             + mua_blood_deoxy(wavelength)*(1-oxy))  # pure blood
+mua = mua_blood*bvf + mua_other
+
+# reduced scattering coefficient ($\mu_s^\prime$ in lab text)
+# the numerical constants are thanks to N. Bashkatov, E. A. Genina and
+# V. V. Tuchin. Optical properties of skin, subcutaneous and muscle
+# tissues: A review. In: J. Innov. Opt. Health Sci., 4(1):9-38, 2011.
+# Units: 1/m
+musr = 100 * (17.6*(wavelength/500)**-4 + 18.78*(wavelength/500)**-0.22)
+
+# mua and musr are now available as shape (3,) arrays
+# Red, green and blue correspond to indexes 0, 1 and 2, respectively
+
+# TODO calculate penetration depth
+skin_depth = np.sqrt(1/(3*(musr + mua)*mua))
+
+coefficients = [["Absorption coefficient (Red)", mua[0]],
+                ["Absorption coefficient (Green)", mua[1]],
+                ["Absorption coefficient (Blue)", mua[2]],
+                ["Reduced scattering coefficient (Red)", musr[0]],
+                ["Reduced scattering coefficient (Green)", musr[1]],
+                ["Reduced scattering coefficient (Blue)", musr[2]]]
+
+print(tabulate(coefficients, headers=["Coefficient", "Value"], tablefmt="grid"))
+penetration_depth = [["Red", skin_depth[0]],
+                     ["Green", skin_depth[1]],
+                     ["Blue", skin_depth[2]]]
+
+print(tabulate(penetration_depth, headers=["Color", "Penetration Depth"], tablefmt="grid"))
