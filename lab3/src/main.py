073cafb5cd914cfbf2a8759f57799fd5d13b8cca
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-03-07 09:40:45 +0100

    refactor: remove unused constants

--- lab3/src/main.py
+++ lab3/src/main.py
@@ -1,12 +1,9 @@
 from preprocessing import txt_to_numpy_array
 from fft import calc_and_plot_fft, colorData_timeplot
 
-N_FFT = 4096*2
-Fs = 30  # 30 frames pr.second
-
 
 def main():
-    data = txt_to_numpy_array("lab3/data/hr_data/close1.txt")
+    data = txt_to_numpy_array("lab3/data/hr_data/close4.txt")
 
     calc_and_plot_fft(data[0])
     calc_and_plot_fft(data[1])
