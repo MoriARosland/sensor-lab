01b16cc2837576154ba5bc5e3ca6a1fc2d9811b2
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-03-07 09:33:00 +0100

    feat: add timeplot

--- lab3/src/main.py
+++ lab3/src/main.py
@@ -1,5 +1,5 @@
 from preprocessing import txt_to_numpy_array
-from fft import calc_and_plot_fft
+from fft import calc_and_plot_fft, colorData_timeplot
 
 N_FFT = 4096*2
 Fs = 30  # 30 frames pr.second
@@ -11,6 +11,10 @@ def main():
     calc_and_plot_fft(data[0])
     calc_and_plot_fft(data[1])
     calc_and_plot_fft(data[2])
+
+    colorData_timeplot(data[0])
+    colorData_timeplot(data[1])
+    colorData_timeplot(data[2])
 
 
 if __name__ == "__main__":
