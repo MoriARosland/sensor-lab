891a622ec8a6b8d3f366fb81314ccef793499543
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-03-07 12:03:29 +0100

    refactor: remove transpose

--- lab3/src/preprocessing.py
+++ lab3/src/preprocessing.py
@@ -4,8 +4,7 @@ from scipy.signal import detrend
 
 def txt_to_numpy_array(txtdata_filepath):
 
-    data = np.loadtxt(txtdata_filepath)
-    dataTransposed = np.transpose(data)
+    data = np.genfromtxt(txtdata_filepath, delimiter=" ")
+    # data = np.transpose(data)
 
-    # Indexing: Red, Green, Blue
-    return dataTransposed
+    return data
