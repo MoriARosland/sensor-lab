8067af9d89983b1584b9b151245448912003caca
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-03-07 12:03:18 +0100

    fix: fft

--- lab3/src/fft.py
+++ lab3/src/fft.py
@@ -1,66 +1,86 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import detrend, butter, filtfilt
-from scipy.fft import fftfreq, fftshift, fft
+from scipy.signal.windows import hann
 
-SAMPLING_RATE = 30  # Hz
-NYQUIST = SAMPLING_RATE / 2
+NFFT = 2**20
 
-# Heart rate range: 30 - 150 BPM
-CUTOFF_LOW = 0.5  # Hz
-CUTOFF_HIGH = 2.5  # Hz
+SAMPLING_FREQ = 40  # Hz
+NYQUIST = SAMPLING_FREQ / 2
+
+# Heart rate range: 30 - 120 BPM
+CUTOFF_LOW = 0.5 / NYQUIST  # Hz normalized
+CUTOFF_HIGH = 3 / NYQUIST  # Hz normalized
 
 
 def calc_and_plot_fft(colorData):
-    window = np.hanning(len(colorData))
+    window = hann(len(colorData))
     colorData = colorData * window
-
     colorData = detrend(colorData)
 
-    f_low = CUTOFF_LOW / NYQUIST
-    f_high = CUTOFF_HIGH / NYQUIST
-    filter_order = 8
+    filter_order = 4
+    b, a = butter(filter_order, Wn=[CUTOFF_LOW, CUTOFF_HIGH], btype='band')
+    colorData = filtfilt(b, a, colorData)
 
-    b, a = butter(filter_order, [f_low, f_high], btype='band')
-    filtered_colorData = filtfilt(b, a, colorData)
+    colorData_fft = np.fft.fft(colorData, NFFT)
+    colorData_fft = np.fft.fftshift(colorData_fft)
 
-    NFFT = 2**20
-    df = SAMPLING_RATE / NFFT
-
-    X = np.fft.fft(filtered_colorData, NFFT)
-    X = np.fft.fftshift(X)
-
+    df = SAMPLING_FREQ / NFFT
     freqs = np.arange(-NYQUIST, NYQUIST, df)
 
-    max_freq = freqs[np.argmax(np.abs(X))]
-    print(f'max heartrate: {np.abs(max_freq * 60)} BPM')
+    spectrum = spectrum = 20 * np.log10(abs(colorData_fft) / max(abs(colorData_fft)))
+
+    middle = int(NFFT / 2)
+    top = 5
+
+    colorData_fft = colorData_fft[middle: int(middle + top / df)]
+    spectrum = spectrum[middle: int(middle + top / df)]
+    freqs = freqs[middle: int(middle + top / df)]
+
+    pulse = freqs[np.argmax(spectrum)] * 60
+    pulse = round(pulse * 100) / 100
+    print(f'Pulse: {pulse} BPM')
+
+    # plt.figure()
+    # plt.plot(freqs, spectrum)
+    # plt.title('Color Data FFT')
+    # plt.xlabel('Frequency (Hz)')
+    # plt.ylabel('Amplitude (dB)')
+    # plt.show()
+
+    return pulse
+
+
+def calc_snr(spectrumData, freqsX):
+    range_start = np.argmax(spectrumData) - 5000
+    range_end = np.argmax(spectrumData) + 5000
+
+    SNR = np.sum(spectrumData[range_start:range_end]) / (
+        np.sum(spectrumData[:range_start]) + np.sum(spectrumData[range_end:])
+    )
+
+    print(f'SNR: {SNR}')
 
     plt.figure()
-    plt.plot(freqs, np.abs(X))
-    plt.title('FFT')
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Magnitude')
+    plt.plot(freqsX, spectrumData)
+    plt.axvline(freqsX[range_start])
+    plt.axvline(freqsX[range_end])
     plt.show()
 
 
 def colorData_timeplot(colorData):
-    window = np.hanning(len(colorData))
-    colorData = colorData * window
-
     colorData = detrend(colorData)
 
-    f_low = CUTOFF_LOW / NYQUIST
-    f_high = CUTOFF_HIGH / NYQUIST
-    filter_order = 8
-
-    b, a = butter(filter_order, [f_low, f_high], btype='band')
+    filter_order = 4
+    b, a = butter(filter_order, [CUTOFF_LOW, CUTOFF_HIGH], btype='band')
     filtered_colorData = filtfilt(b, a, colorData)
 
-    time = np.arange(0, len(colorData) / SAMPLING_RATE, 1/SAMPLING_RATE)
+    time = np.arange(0, len(colorData) / SAMPLING_FREQ, 1/SAMPLING_FREQ)
 
     plt.figure()
+    plt.plot(time, colorData)
     plt.plot(time, filtered_colorData)
-    plt.title('Color Data')
+    plt.title('Time plot')
     plt.xlabel('Time (s)')
     plt.ylabel('Amplitude')
     plt.show()
--- lab3/src/main.py
+++ lab3/src/main.py
@@ -1,17 +1,43 @@
+import os
+import numpy as np
 from preprocessing import txt_to_numpy_array
 from fft import calc_and_plot_fft, colorData_timeplot
 
+START = 500
+END = -1
+
 
 def main():
-    data = txt_to_numpy_array("lab3/data/hr_data/close4.txt")
+    data = txt_to_numpy_array("/Users/rosland/Documents/sensor-lab/lab-3/data/reflektans/four.txt")
+    # data = txt_to_numpy_array("lab3/data/hr_data/Transmittans-2.txt")
 
-    calc_and_plot_fft(data[0])
-    calc_and_plot_fft(data[1])
-    calc_and_plot_fft(data[2])
+    # calc_and_plot_fft(data[START:END, 0])
+    # calc_and_plot_fft(data[START:END, 1])
+    # calc_and_plot_fft(data[START:END, 2])
 
-    colorData_timeplot(data[0])
-    colorData_timeplot(data[1])
-    colorData_timeplot(data[2])
+    # colorData_timeplot(data[START:END, 0])
+    # colorData_timeplot(data[START:END, 1])
+    # colorData_timeplot(data[START:END, 2])
+
+    path = "/Users/rosland/development/sensor-lab/lab3/data/hr_reflektans/"
+    path = "/Users/rosland/development/sensor-lab/lab3/data/hr_transmittans/"
+
+    for i in range(3):
+        pulses = [
+            calc_and_plot_fft(
+                txt_to_numpy_array(f"{path}{file}")[START:END, i]
+            )
+            for file in os.listdir(path)
+            if file != ".DS_Store"
+        ]
+
+        pulses = sorted(pulses)
+
+        avg = round(np.mean(pulses), 2)
+        print(f"Average pulse:{avg}")
+
+        std = round(np.std([abs(el) for el in pulses]), 4)
+        print(f"Standard deviation from pulses:{std}")
 
 
 if __name__ == "__main__":
